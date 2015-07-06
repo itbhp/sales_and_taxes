c8b23101e1e99bd9b69fb675b9c0fd9970571c7a
Paolo Sciarra <itbhps@gmail.com>
2015-07-06 19:43:08 +0200

    refactored parser

--- src/main/java/it/twinsbrain/sales/and/taxes/parser/ProductParser.java
+++ src/main/java/it/twinsbrain/sales/and/taxes/parser/ProductParser.java
@@ -5,13 +5,9 @@ import static it.twinsbrain.sales.and.taxes.parser.ProductParser.ProdcutType.FOO
 import static it.twinsbrain.sales.and.taxes.parser.ProductParser.ProdcutType.MEDICAL;
 import static it.twinsbrain.sales.and.taxes.parser.ProductParser.ProdcutType.MUSIC;
 import static it.twinsbrain.sales.and.taxes.parser.ProductParser.ProdcutType.OTHERS;
-import java.util.Arrays;
 import static java.util.Arrays.asList;
 import java.util.Collections;
-import static java.util.Collections.emptyList;
-import java.util.EnumMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  *
@@ -19,37 +15,37 @@ import java.util.Map;
  */
 public class ProductParser {
 
-    private final EnumMap<ProdcutType, List<String>> descriptionsByProduct;
-
-    public ProductParser() {
-        descriptionsByProduct = new EnumMap<>(ProdcutType.class);
-        descriptionsByProduct.put(FOOD, asList("chocolate", "bread"));
-        descriptionsByProduct.put(BOOK, asList("book"));
-        descriptionsByProduct.put(MEDICAL, asList("pills"));
-        descriptionsByProduct.put(MUSIC, asList("cd","mp3"));
-        descriptionsByProduct.put(OTHERS, emptyList());
-
-    }
-
     public int readQuantity(String input) {
         String[] parts = input.split(" ");
         return Integer.parseInt(parts[0]);
     }
 
     public ProdcutType readProdcutType(String input) {
-        for(Map.Entry<ProdcutType,List<String>> entry: descriptionsByProduct.entrySet()){
-            for(String description: entry.getValue()){
-                if(input.toLowerCase().contains(description)){
-                    return entry.getKey();
-                }
+        for (ProdcutType type : ProdcutType.values()) {
+            if(type.match(input)){
+                return type;
             }
         }
-        return ProdcutType.OTHERS;
+        return OTHERS;
     }
 
     public static enum ProdcutType {
 
-        FOOD, BOOK, MUSIC, MEDICAL, OTHERS;
+        FOOD(asList("chocolate", "bread")),
+        BOOK(asList("book")),
+        MUSIC(asList("cd", "mp3")),
+        MEDICAL(asList("pills")),
+        OTHERS(Collections.<String>emptyList());
+
+        ProdcutType(List<String> keywords) {
+            this.keywords = keywords;
+        }
+
+        private final List<String> keywords;
+
+        public boolean match(String productDescription) {
+            return keywords.stream().anyMatch((keyword) -> (productDescription.contains(keyword)));
+        }
 
     }
 
