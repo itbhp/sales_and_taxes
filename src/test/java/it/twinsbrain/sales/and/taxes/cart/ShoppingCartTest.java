5b56d7235271a81472f494019c8d560dd06a70e2
Paolo Sciarra <itbhps@gmail.com>
2015-07-11 18:31:38 +0200

    added assertions

--- src/test/java/it/twinsbrain/sales/and/taxes/cart/ShoppingCartTest.java
+++ src/test/java/it/twinsbrain/sales/and/taxes/cart/ShoppingCartTest.java
@@ -4,6 +4,11 @@ import it.twinsbrain.sales.and.taxes.parser.CartItemParser;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import static it.twinsbrain.sales.and.taxes.cart.ProductType.MUSIC;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -30,7 +35,14 @@ public class ShoppingCartTest {
             underTest.read(input);
         }
         then:{
-            assertThat(underTest.list().size(), is(1));
+            List<CartItem> cartItems = underTest.list();
+            assertThat(cartItems.size(), is(1));
+
+            CartItem cartItem = cartItems.get(0);
+            assertThat(cartItem.quantity, is(1));
+            assertThat(cartItem.description, is(equalTo("music cd")));
+            assertThat(cartItem.price, is(equalTo(new BigDecimal("12.99"))));
+            assertThat(cartItem.type, is(MUSIC));
         }
     }
 }
