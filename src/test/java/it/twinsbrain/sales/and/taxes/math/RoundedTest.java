32b03c9868cebe3ddb817378cd800fce10d24c04
Paolo Sciarra <itbhps@gmail.com>
2015-07-14 22:41:17 +0200

    added rounded tests

--- src/test/java/it/twinsbrain/sales/and/taxes/math/RoundedTest.java
+++ src/test/java/it/twinsbrain/sales/and/taxes/math/RoundedTest.java
@@ -1,8 +1,6 @@
 package it.twinsbrain.sales.and.taxes.math;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import net.avh4.test.junit.Nested;
 
 import java.math.BigDecimal;
 
@@ -13,27 +11,43 @@ import static org.junit.Assert.assertThat;
 /**
  * @author angelosciarra
  */
-@RunWith(Nested.class)
 public class RoundedTest {
 
-    private static Rounded underTest;
-    private static BigDecimal input;
+    private Rounded underTest;
+    private BigDecimal input;
 
-    static class RoundUpToTwoCentsTest {
 
-        @Test
-        public void it_should_round_to_the_nearest_two_cents(){
-            given:{
-                input = new BigDecimal("17.003");
-            }
-            when:{
-                underTest = new Rounded(input, "2", "100");
-            }
-            then: {
-                assertThat(underTest.getValue(), is(equalTo(new BigDecimal("17.02"))));
-            }
+    @Test
+    public void it_should_round_to_the_nearest_two_cents() {
+        given:
+        {
+            input = new BigDecimal("17.033");
         }
-
+        when:
+        {
+            underTest = new Rounded(input, "2", "100");
+        }
+        then:
+        {
+            assertThat(underTest.getValue(), is(equalTo(new BigDecimal("17.04"))));
+        }
     }
+
+    @Test
+    public void it_should_round_to_the_nearest_five_cents() {
+        given:
+        {
+            input = new BigDecimal("17.033");
+        }
+        when:
+        {
+            underTest = new Rounded(input, "5", "100");
+        }
+        then:
+        {
+            assertThat(underTest.getValue(), is(equalTo(new BigDecimal("17.05"))));
+        }
+    }
+
 
 }
