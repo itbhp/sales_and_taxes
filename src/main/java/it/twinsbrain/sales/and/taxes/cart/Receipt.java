256eb1e6f710244324e679d1d4ca8d155115d39e
Paolo Sciarra <paolo.sciarra@rumbo.com>
2015-07-15 14:05:35 +0200

    changed apache commons lang version, variable renaming

--- src/main/java/it/twinsbrain/sales/and/taxes/cart/Receipt.java
+++ src/main/java/it/twinsbrain/sales/and/taxes/cart/Receipt.java
@@ -13,9 +13,10 @@ public class Receipt {
     }
 
     public String print(){
-        String receipt = itemsWithTaxes.stream().map(CartItem::toString).reduce("", (a, b) -> a + b + "\n");
-        BigDecimal taxesSum = itemsWithTaxes.stream().map(c -> c.taxes).reduce(BigDecimal.ZERO, BigDecimal::add);
-        BigDecimal totalPrice = itemsWithTaxes.stream().map(c -> c.priceWithTaxes).reduce(BigDecimal.ZERO, BigDecimal::add);
+        String receipt = itemsWithTaxes.stream().map(CartItem::toString).reduce("", (acc, curr) -> acc + curr + "\n");
+        BigDecimal taxesSum = itemsWithTaxes.stream().map(i -> i.taxes).reduce(BigDecimal.ZERO, BigDecimal::add);
+        BigDecimal totalPrice = itemsWithTaxes.stream().map(i -> i.priceWithTaxes).reduce(BigDecimal.ZERO, BigDecimal::add);
+
         receipt += "Sales Taxes: "+taxesSum.setScale(2,BigDecimal.ROUND_HALF_UP)+"\n";
         receipt += "Total: "+totalPrice.setScale(2,BigDecimal.ROUND_HALF_UP);
         return receipt;
