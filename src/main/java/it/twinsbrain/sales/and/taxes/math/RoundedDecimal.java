3b7cc639dfc35dbdbd88498e1e8bb8bceb444419
Paolo Sciarra <itbhps@gmail.com>
2021-10-10 22:36:16 +0200

    minor refactoring

--- src/main/java/it/twinsbrain/sales/and/taxes/math/RoundedDecimal.java
+++ src/main/java/it/twinsbrain/sales/and/taxes/math/RoundedDecimal.java
@@ -1,21 +1,22 @@
 package it.twinsbrain.sales.and.taxes.math;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class RoundedDecimal {
 
     private final BigDecimal value;
 
     public RoundedDecimal(final BigDecimal orig, final String nearestUnit, final String unitBase) {
-        final BigDecimal roundFactor = new BigDecimal(unitBase).divide(new BigDecimal(nearestUnit));
-        value = ceil(orig.multiply(roundFactor)).divide(roundFactor);
+        final BigDecimal roundFactor = new BigDecimal(unitBase).divide(new BigDecimal(nearestUnit), 3, RoundingMode.HALF_UP);
+        value = ceilUp(orig.multiply(roundFactor)).divide(roundFactor, 3, RoundingMode.HALF_UP);
     }
 
     public BigDecimal value() {
         return value;
     }
 
-    private BigDecimal ceil(final BigDecimal value) {
-        return value.setScale(0, BigDecimal.ROUND_UP);
+    private BigDecimal ceilUp(final BigDecimal value) {
+        return value.setScale(0, RoundingMode.UP);
     }
 }
