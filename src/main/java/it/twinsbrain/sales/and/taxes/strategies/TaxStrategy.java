7ab5693ecad24b70b636010bf7cd8f37f522d2e6
Paolo Sciarra <itbhps@gmail.com>
2021-10-17 09:59:09 +0200

    let's try starting fresh

--- src/main/java/it/twinsbrain/sales/and/taxes/strategies/TaxStrategy.java
+++
@@ -1,8 +0,0 @@
-package it.twinsbrain.sales.and.taxes.strategies;
-
-import it.twinsbrain.sales.and.taxes.cart.CartItem;
-
-public interface TaxStrategy {
-
-    CartItem updateTaxesOn(final CartItem visitee);
-}
